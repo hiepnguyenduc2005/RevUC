defcff3982bf2f8d3f3822fe9698f382d3652a0b
hiepnguyenduc2005 <hiepnguyenduc2005@gmail.com>
2025-03-01 16:56:03 -0500

    db connect signup

--- backend/api/endpoints.py
+++ backend/api/endpoints.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, HTTPException, Query
 from db.connect import org_collection, user_collection, match_collection
+from models.schema import Signup, Organization
+import bcrypt
+from bson import ObjectId
 
 router = APIRouter()
 
@@ -7,13 +10,38 @@ router = APIRouter()
 def get_root():
     return {"message": "Welcome to RevUC!"}
 
+def hash_password(password: str) -> str:
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode(), salt).decode()
+
 @router.post("/login-org")
-def login():
+def login_org():
+    # TODO
     return {"message": "Login Organization"}
 
 @router.post("/signup-org")
-def signup():
-    return {"message": "Signup Organization"}
+async def signup_org(org: Signup):
+    # TODO
+    existing_user = await org_collection.find_one({"$or": [{"username": org.username}, {"email": org.email}]})
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username or Email already exists.")
+
+    hashed_password = hash_password(org.password)
+
+    new_org = Organization(
+        name=org.name,
+        username=org.username,
+        password=hashed_password,
+        email=org.email,
+        trials=[]
+    ).dict()
+
+    result = await org_collection.insert_one(new_org)
+    return {"message": "Signup successful", "org_id": str(result.inserted_id)}
+
+def create_user(user):
+    # TODO
+    return user_collection.insert_one(user)
 
 @router.post("/trials")
 def create_trial():
